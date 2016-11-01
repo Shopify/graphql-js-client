e546618f8a3bf4464fb8cee1e351f1397d835c1b
mikkoh <mikko.haapoja@shopify.com>
2016-11-01 16:11:13 -0400

    Removed checks for process.env.NODE_ENV === 'production' from validate-input-value

--- src/validate-input-value.js
+++ src/validate-input-value.js
@@ -1,42 +1,33 @@
 import validateType from './validate-type';
 
+
+function throwTypeMismatch(expectedType) {
+  throw new Error(`The default value is not of type ${expectedType}`);
+}
+
+function isFloat(floatValue) {
+  const valueString = floatValue.toString().toLowerCase();
+
+  return valueString.includes('.') || valueString.includes('e');
+}
+
+const graphTypeToJSType = {
+  String,
+  Boolean,
+  Int: Number,
+  Float: Number
+};
+
 export default (type, value) => {
-  try {
-    if (process.env.NODE_ENV !== 'production') {
-      // eslint-disable-next-line no-inner-declarations
-      function throwTypeMismatch(expectedType) {
-        throw new Error(`The default value is not of type ${expectedType}`);
-      }
+  if (type[type.length - 1] === '!') {
+    throw new Error('You cannot use a default value when using a non-null type');
+  }
 
-      // eslint-disable-next-line no-inner-declarations
-      function isFloat(floatValue) {
-        const valueString = floatValue.toString().toLowerCase();
-
-        return valueString.includes('.') || valueString.includes('e');
-      }
-
-      const graphTypeToJSType = {
-        String,
-        Boolean,
-        Int: Number,
-        Float: Number
-      };
-
-      if (type[type.length - 1] === '!') {
-        throw new Error('You cannot use a default value when using a non-null type');
-      }
-
-      if (graphTypeToJSType[type]) {
-        if (!validateType(value, graphTypeToJSType[type])) {
-          throwTypeMismatch(type);
-        } else if (type === 'Int' && isFloat(value)) {
-          throwTypeMismatch(type);
-        }
-      }
-    }
-  } catch (error) {
-    if (error.message !== 'process is not defined') {
-      throw error;
+  if (graphTypeToJSType[type]) {
+    if (!validateType(value, graphTypeToJSType[type])) {
+      throwTypeMismatch(type);
+    } else if (type === 'Int' && isFloat(value)) {
+      throwTypeMismatch(type);
     }
   }
 };

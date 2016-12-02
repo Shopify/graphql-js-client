21d146f27fe3cc1a4f25614b9843e95a0b605772
Mina Smart <mina.smart@shopify.com>
2016-12-02 16:18:48 -0500

    don't explicitly add fields since our we look for the node interface and add it now

--- src/deserialize-object.js
+++ src/deserialize-object.js
@@ -86,7 +86,6 @@ function deserializeValue(value, selectionSet, registry, parent) {
           root.addField('node', {id: rootNodeId}, (node) => {
             node.addInlineFragmentOn(rootNodesSelectionSet.typeSchema.name, (rootNode) => {
               if (chain.length) {
-                rootNode.addField('id');
                 addNextFieldTo(rootNode, chain.shift(), rootNodesSelectionSet.selections, chain);
               } else {
                 const fieldReference = rootNodesSelectionSet.selections.find((field) => {
@@ -104,7 +103,6 @@ function deserializeValue(value, selectionSet, registry, parent) {
                 });
 
                 // Traverse the sets. This is the connection's set
-                rootNode.addField('id');
                 rootNode.addConnection(fieldName, args, nodeField.selectionSet);
               }
             });
@@ -224,7 +222,7 @@ export default function deserializeObject(data, selectionSet, registry = new Cla
     if (ancestry.isNode) {
       attrs.refetchQuery = function() {
         return new Query(selectionSet.typeBundle, (root) => {
-          root.addField('node', {id: this.ancestry.nodeId}, (node) => {
+          root.add('node', {id: this.ancestry.nodeId}, (node) => {
             node.addInlineFragmentOn(this.ancestry.selectionSet.typeSchema.name, this.ancestry.selectionSet);
           });
         });

e4034756c61203cf0990c5e35fbf237b9667d072
jzjiang <jessica.jiang@shopify.com>
2017-04-24 15:38:01 -0400

    Add jsdoc annotations

--- src/class-registry.js
+++ src/class-registry.js
@@ -1,18 +1,38 @@
 import GraphModel from './graph-model';
 
+/**
+ * A registry of classes used to serialize the response data. Uses {@link GraphModel} by default.
+ */
 export default class ClassRegistry {
   constructor() {
     this.classStore = {};
   }
 
+  /**
+   * Registers a class for a GraphQL type in the registry.
+   *
+   * @param {Class} constructor The constructor of the class.
+   * @param {String} type The GraphQL type of the object to serialize into the class.
+   */
   registerClassForType(constructor, type) {
     this.classStore[type] = constructor;
   }
 
+  /**
+   * Unregisters a class for a GraphQL type in the registry.
+   *
+   * @param {String} type The GraphQL type to unregister.
+   */
   unregisterClassForType(type) {
     delete this.classStore[type];
   }
 
+  /**
+   * Returns the class for the given GraphQL type.
+   *
+   * @param {String} type The GraphQL type to look up.
+   * @return {Class|GraphModel} The class for the given GraphQL type. Defaults to {@link GraphModel} if no class is registered for the GraphQL type.
+   */
   classForType(type) {
     return this.classStore[type] || GraphModel;
   }
--- src/variable.js
+++ src/variable.js
@@ -1,6 +1,15 @@
 import formatInputValue from './format-input-value';
 
 export class VariableDefinition {
+
+  /**
+   * This constructor should not be invoked directly.
+   * Use the factory function {@link Client#variable} to create a VariableDefinition.
+   *
+   * @param {String} name The name of the variable.
+   * @param {String} type The GraphQL type of the variable.
+   * @param {*} [defaultValue] The default value of the variable.
+   */
   constructor(name, type, defaultValue) {
     this.name = name;
     this.type = type;
@@ -8,10 +17,20 @@ export class VariableDefinition {
     Object.freeze(this);
   }
 
+  /**
+   * Returns the GraphQL query string for the variable as an input value (e.g. `$variableName`).
+   *
+   * @return {String} The GraphQL query string for the variable as an input value.
+   */
   toInputValueString() {
     return `$${this.name}`;
   }
 
+  /**
+   * Returns the GraphQL query string for the variable (e.g. `$variableName:VariableType = defaultValue`).
+   *
+   * @return {String} The GraphQL query string for the variable.
+   */
   toString() {
     const defaultValueString = this.defaultValue ? ` = ${formatInputValue(this.defaultValue)}` : '';
 

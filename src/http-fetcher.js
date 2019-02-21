1a076e9aaa8b6bdf278bc7307d555d91bc188b01
Cameron Dubas <camerondubas@gmail.com>
2019-02-21 12:49:15 -0800

    Remove response helper methods in favour of inline handling

--- src/http-fetcher.js
+++ src/http-fetcher.js
@@ -11,20 +11,15 @@ export default function httpFetcher(url, options = {}) {
         ...options.headers,
         ...headers
       }
-    }).then((response) => _handleResponse(response));
+    }).then((response) => {
+      const contentType = response.headers.get('content-type');
+
+      if (contentType.includes('application/json')) {
+        return response.json();
+      }
+
+      return response.text()
+        .then((responseText) => ({text: responseText}));
+    });
   };
-}
-
-function _handleResponse(response) {
-  const contentType = response.headers.get('content-type');
-
-  if (contentType.includes('application/json')) {
-    return response.json();
-  }
-
-  return _handleTextResponse(response);
-}
-
-function _handleTextResponse(response) {
-  return response.text().then((responseText) => ({text: responseText}));
 }
